76e7eae73c12a4f1877f444358565d8cfca150a7
Mysteriouscoder5 <achrekaratharva5@gmail.com>
2024-08-22 11:32:18 +0530

    backend folder structure

---
+++ backend/src/app.js
@@ -0,0 +1,54 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config/config.env" });
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("path");
+
+const userRoute = require("./routes/v1/User");
+require("./db/connection");
+
+const port = process.env.PORT;
+
+// handling uncaught exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error : ${err.message}`);
+  console.log("Shutting down the server due to uncaught exception");
+  process.exit(1);
+});
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+async function serveStatic() {
+  app.use(express.static(path.join(__dirname, "./frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./frontend/dist"));
+  });
+}
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
+app.use("/api/v1/user", userRoute);
+
+const server = app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+  serveStatic();
+});
+
+// unhandled rejection problem
+process.on("unhandledRejection", (err) => {
+  console.log(`Error : ${err}`);
+  console.log("Shutting down the server due to unhandled rejection");
+  server.close(() => {
+    process.exit(1);
+  });
+});
