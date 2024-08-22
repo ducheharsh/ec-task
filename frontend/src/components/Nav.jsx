33f3eed87a3fbeb615fe79041241a818a82b6aad
Adhar1777 <149721236+AdharBattulwar@users.noreply.github.com>
2024-08-22 13:10:46 +0530

    Frontend Init

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -4,42 +4,42 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <>
-    <div className='navWrapper flex items-center justify-between absolute h-[3rem] w-full bg-red-400 px-52'>
+    <div className='navWrapper flex items-center justify-between relative h-[3rem] w-full bg-gray-700 px-52'>
         <div className='navLeft flex items-center justify-center'>
             <div className='navIcon'>
                 <img src="" alt='img'></img>
             </div>
         </div>
         <div className='navRight flex items-center justify-center'>
-            <div className='navComponents'>
+            <div className='navComponents flex items-center justify-between gap-4'>
                 <Link to="/" >
                     <div className='navComponentsHome'>
-                        
+                        Home
                     </div>
                 </Link>
                 <Link to="/" >
-                    <div className='navComponents'>
-                        
+                    <div className='navComponentsMyNetwork'>
+                        My Network
                     </div>
                 </Link>
                 <Link to="/" >
-                    <div className='navComponents'>
-                        
+                    <div className='navComponentsJobs'>
+                        Jobs
                     </div>
                 </Link>
                 <Link to="/" >
-                    <div className='navComponents'>
-                        
+                    <div className='navComponentsMesseging'>
+                        Messaging
                     </div>
                 </Link>
                 <Link to="/" >
-                    <div className='navComponents'>
-                        
+                    <div className='navComponentsNotification'>
+                        Notification
                     </div>
                 </Link>
                 <Link to="/" >
-                    <div className='navComponents'>
-                        
+                    <div className='navComponentsProfile'>
+                        Profile
                     </div>
                 </Link>
             </div>
